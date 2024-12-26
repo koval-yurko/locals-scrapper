4951582deffe8904fa5b2bbea61156af4136b4b4
Yurii Kovalchuk <failwin@ukr.net>
2024-12-26 15:36:36 +0200

    Initial commit

---
+++ test/locals-scrapper-infra.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as LocalsScrapperInfra from '../lib/locals-scrapper-infra-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/locals-scrapper-infra-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new LocalsScrapperInfra.LocalsScrapperInfraStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
